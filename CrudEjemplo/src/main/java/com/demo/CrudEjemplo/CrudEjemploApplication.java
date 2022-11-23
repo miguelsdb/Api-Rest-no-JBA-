ebe468d4f571ead8149ccd8ccd819d6b8433db82
Miguel Santiago Deavila Barrera <sdeavila220@gmail.com>
2022-11-23 13:15:42 -0500

    first commit

---
+++ CrudEjemplo/src/main/java/com/demo/CrudEjemplo/CrudEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.demo.CrudEjemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEjemploApplication.class, args);
+	}
+
+}
